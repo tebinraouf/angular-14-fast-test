0676e55c149fd4bf3e4fd96c0c84bc865cf2cbdd
ManthiriyappanAruchamyTR <manthiriyappan.aruchamy@thomsonreuters.com>
2023-03-03 22:58:32 +0530

    Initiall Code setup for fastdesign test

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {​​​​​​ CUSTOM_ELEMENTS_SCHEMA }​​​​​​ from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
